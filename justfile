fdac27d99c2bc395a967a56a82d308de1cc0c66a
Xiatong Zheng <xiatong.zheng@deepl.com>
2023-04-22 00:48:03 +0200

    refactor: move commands to script

--- justfile
+++
@@ -1,8 +0,0 @@
-context:
-    kubectl config view
-    kubectl config current-context
-switch-context cluster:
-    kubectl config use-context {{cluster}}
-
-explain:
-    kubectl explain pods --recursive
---
+++ kubernetes_commands.sh
@@ -0,0 +1,34 @@
+#kubectl api-resources
+
+#kubectl get pods
+kubectl get pods --show-labels
+#kubectl get namespaces
+#kubectl get svc
+#kubectl get svc -n kube-system
+#kubectl get svc --all-namespaces
+#kubectl get deploy hello-deploy
+#kubectl get rs
+
+#kubectl describe ns default
+#kubectl describe deploy hello-deploy
+
+#kubectl explain pods --recursive
+
+#kubectl create ns hydra
+
+#kubectl delete ns shield
+kubectl delete -f deployments/svc.yml
+
+#kubectl config view
+#kubectl config current-context
+#kubectl config use-context docker-desktop
+#kubectl config set-context --current --namespace default
+
+#kubectl apply -f deployments/deploy.yml
+
+#kubectl rollout status deployment hello-deploy
+#kubectl rollout pause deploy hello-deploy
+#kubectl rollout history deployment hello-deploy
+#kubectl rollout undo deployment hello-deploy --to-revision=1
+
+#kubectl scale deploy hello-deploy --replicas 5
