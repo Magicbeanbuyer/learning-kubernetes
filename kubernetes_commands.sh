d8e2ebe3cc9e8b5477199c1f5782cdc4dcf6396c
Xiatong Zheng <xiatong.zheng@deepl.com>
2023-04-30 19:17:18 +0200

    feat: add services

--- kubernetes_commands.sh
+++ kubernetes_commands.sh
@@ -1,23 +1,25 @@
 #kubectl api-resources
 
 #kubectl get pods
-kubectl get pods --show-labels
+#kubectl get pods --show-labels
 #kubectl get namespaces
 #kubectl get svc
 #kubectl get svc -n kube-system
 #kubectl get svc --all-namespaces
 #kubectl get deploy hello-deploy
 #kubectl get rs
+#kubectl get endpointslices
 
 #kubectl describe ns default
 #kubectl describe deploy hello-deploy
+kubectl describe endpointslice svc-test-js48c
 
 #kubectl explain pods --recursive
 
 #kubectl create ns hydra
 
 #kubectl delete ns shield
-kubectl delete -f deployments/svc.yml
+#kubectl delete -f deployments/svc.yml
 
 #kubectl config view
 #kubectl config current-context
@@ -32,3 +34,5 @@ kubectl delete -f deployments/svc.yml
 #kubectl rollout undo deployment hello-deploy --to-revision=1
 
 #kubectl scale deploy hello-deploy --replicas 5
+
+#kubectl expose deployment svc-test --type=NodePort
